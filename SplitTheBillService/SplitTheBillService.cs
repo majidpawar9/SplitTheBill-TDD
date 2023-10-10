64d422aed5eb569d6902b2a6e981971973dad5c9
majidpawar9 <majidpawar9@gmail.com>
2023-10-09 20:14:42 -0400

    added service class

---
+++ SplitTheBillService/SplitTheBillService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SplitTheBill.Service
+{
+    public class STBService
+    {
+        public bool IsBill(int amount)
+        {
+            throw new NotImplementedException("Please create a test first.");
+        }
+    }
+}
