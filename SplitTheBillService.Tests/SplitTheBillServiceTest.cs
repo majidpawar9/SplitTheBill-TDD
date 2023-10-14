dd009703d407804a7106f9120c0dd433e0298b73
majidpawar9 <majidpawar9@gmail.com>
2023-10-13 21:57:51 -0400

    Implemented CalculateTips func and its Tests

--- SplitTheBillService.Tests/SplitTheBillServiceTest.cs
+++ SplitTheBillService.Tests/SplitTheBillServiceTest.cs
@@ -19,7 +19,6 @@ namespace STB.UnitTests.Services
         [DataRow(100.00, 4, 25.00)] // Total amount divided by 4 people should be 25.00
         [DataRow(50.00, 2, 25.00)]  // Total amount divided by 2 people should be 25.00
         [DataRow(100.00, 1, 100.00)] // When there's only 1 person, they should pay the full amount
-
         public void SplitAmount_ReturnsCorrectSplit(double totalAmount, int numberOfPeople, double expectedSplit)
         {
 
@@ -28,6 +27,7 @@ namespace STB.UnitTests.Services
 
             Assert.AreEqual(expectedSplit, splitAmount);
         }
+
         [TestMethod]
         [DataRow(-100.00, 4)]  // Total amount is negative
         [DataRow(100.00, -4)]  // Number of people is negative
@@ -38,12 +38,50 @@ namespace STB.UnitTests.Services
             Action act = () => _STBService.SplitAmount(totalAmount, numberOfPeople);
             Assert.ThrowsException<Exception>(act);
         }
+
         [TestMethod]
         [DataRow(0.00, 4)]
         public void SplitAmount_ZeroAmount_ReturnZero_IrrespectiveOfNoOfPeople(double totalAmount, int numberOfPeople)
         {
             double splitAmount = _STBService.SplitAmount(totalAmount, numberOfPeople);
             Assert.AreEqual(0, splitAmount);
+        }
+
+        [TestMethod]
+        [DataRow(20.00, 15, 3.0)] // Meal cost, tip percentage, expected tip amount
+        [DataRow(15.00, 50, 7.50)]
+        [DataRow(25.00, 10, 2.5)]
+        public void CalculateTips_ValidInput_ReturnsCorrectTipAmounts(double mealCost, float tipPercentage, double expectedTip)
+        {
+            var mealCosts = new Dictionary<string, double>
+        {
+            { "People", mealCost },
+        };
+            var tipAmounts = _STBService.CalculateTips(mealCosts, tipPercentage);
+
+            Assert.AreEqual(expectedTip, tipAmounts["People"]);
+            
+        }
+
+        [TestMethod]
+        [DataRow(null, 15)]
+        public void CalculateTips_NullMealCosts_ThrowsException(Dictionary<string, double> mealCosts, float tipPercentage)
+        {
+            Assert.ThrowsException<Exception>(() => _STBService.CalculateTips(mealCosts, tipPercentage));
+        }
+
+        [TestMethod]
+        [DataRow(100.00, -10)]
+        public void CalculateTips_NegativeTipPercentage_ThrowsException(double mealCost, float tipPercentage)
+        {
+            // Arrange
+            var mealCosts = new Dictionary<string, double>
+        {
+            { "Person", mealCost }
+        };
+
+            // Act and Assert
+            Assert.ThrowsException<Exception>(() => _STBService.CalculateTips(mealCosts, tipPercentage));
         }
 
     }
--- SplitTheBillService/SplitTheBillService.cs
+++ SplitTheBillService/SplitTheBillService.cs
@@ -17,8 +17,34 @@ namespace SplitTheBill.Service
             }
 
             double split = totalAmount / numberOfPeople;
-            
+
             return split;
+        }
+
+        public Dictionary<string, double> CalculateTips(Dictionary<string, double> mealCosts, float tipPercentage)
+        {
+            if (mealCosts == null || mealCosts.Count == 0)
+            {
+                throw new Exception("The meal costs dictionary cannot be null or empty.");
+            }
+
+            if (tipPercentage < 0)
+            {
+                throw new Exception("The tip percentage cannot be negative.");
+            }
+
+            double totalMealCost = mealCosts.Values.Sum();
+            double totalTip = (double)tipPercentage / 100 * totalMealCost;
+
+            var tipAmounts = new Dictionary<string, double>();
+
+            foreach (var entry in mealCosts)
+            {
+                double tip = entry.Value / totalMealCost * totalTip;
+                tipAmounts[entry.Key] = tip;
+            }
+
+            return tipAmounts;
         }
     }
 }
